ae0dad6a580faa9da4a92bf02c9d7abb0913f7ca
tmsvspn <82614363+tmsvspn@users.noreply.github.com>
2021-04-27 19:32:08 +0200

    Replace Random with SecureRandom in TorpedoStore
    This is for security reasons, as requested by timiant
    in the recent pull request

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -1,6 +1,6 @@
 package hu.bme.mit.spaceship;
 
-import java.util.Random;
+import java.security.SecureRandom;
 
 /**
 * Class storing and managing the torpedoes of a ship
@@ -14,7 +14,7 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
-  private Random generator = new Random();
+  private SecureRandom generator = new SecureRandom();
 
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
