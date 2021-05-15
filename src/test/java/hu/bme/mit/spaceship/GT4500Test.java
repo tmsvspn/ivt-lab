39431cb1b949ee4ca44395e3e0ef281b224994ab
tmsvspn <82614363+tmsvspn@users.noreply.github.com>
2021-05-15 17:33:12 +0200

    Update Tests which use mock

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,17 +9,21 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore ts1;
+  private TorpedoStore ts2;
 
   @BeforeEach
   public void init(){
-    TorpedoStore ts1 = mock(TorpedoStore.class, withSettings().useConstructor(10));
-    TorpedoStore ts2 = mock(TorpedoStore.class, withSettings().useConstructor(10));
-    this.ship = new GT4500(ts1, ts2);
+    this.ts1 = mock(TorpedoStore.class, withSettings().useConstructor(10));
+    this.ts2 = mock(TorpedoStore.class, withSettings().useConstructor(10));
+    this.ship = new GT4500(this.ts1, this.ts2);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(ts1.fire(1)).thenReturn(true);
+    when(ts2.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
@@ -31,6 +35,8 @@ public class GT4500Test {
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(ts1.fire(1)).thenReturn(true);
+    when(ts2.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
