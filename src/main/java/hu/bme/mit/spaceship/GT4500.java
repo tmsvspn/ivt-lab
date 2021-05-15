7e0679fad74318fc5f0aa5f9462880ece06ee140
tmsvspn <82614363+tmsvspn@users.noreply.github.com>
2021-05-15 17:20:10 +0200

    Update constructor of GT4500 to use dependency injection

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -10,9 +10,9 @@ public class GT4500 implements hu.bme.mit.spaceship.SpaceShip {
 
   private boolean wasPrimaryFiredLast = false;
 
-  public GT4500() {
-    this.primaryTorpedoStore = new hu.bme.mit.spaceship.TorpedoStore(10);
-    this.secondaryTorpedoStore = new hu.bme.mit.spaceship.TorpedoStore(10);
+  public GT4500(TorpedoStore primaryTorpedoStore, TorpedoStore secondaryTorpedoStore) {
+    this.primaryTorpedoStore = primaryTorpedoStore;
+    this.secondaryTorpedoStore = secondaryTorpedoStore;
   }
 
   public boolean fireLaser(hu.bme.mit.spaceship.FiringMode firingMode) {
