aaf9479bd3b219a6578a0aa44d22456916c7785c
tmsvspn <82614363+tmsvspn@users.noreply.github.com>
2021-05-15 22:26:12 +0200

    Add 5+1 Tests

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -46,4 +46,76 @@ public class GT4500Test {
     verify(mockTS2, times(1)).fire(anyInt());
   }
 
+  @Test
+  public void fireTorpedo_Single_Alternate_Second() {
+    // Arrange
+    when(mockTS1.fire(1)).thenReturn(true);
+    when(mockTS2.fire(1)).thenReturn(true);
+    // Egyet lovunk, es azt tudjuk, hogy az elso loves az elsobol jon
+    ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Act
+    ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    verify(mockTS2, times(1)).fire(anyInt());
+  }
+
+  @Test
+  public void fireTorpedo_Single_Alternate_First() {
+    // Arrange
+    when(mockTS1.fire(1)).thenReturn(true);
+    when(mockTS2.fire(1)).thenReturn(true);
+    // Egyet lovunk, es azt tudjuk, hogy az elso loves az elsobol jon
+    ship.fireTorpedo(FiringMode.SINGLE);
+    // Lovunk meg egyet, es azt tudjuk, hogy a masodik loves a masodikbol jon
+    ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Act
+    ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    verify(mockTS1, times(2)).fire(anyInt());
+  }
+
+  @Test
+  public void fireTorpedo_Single_Empty() {
+    // Arrange
+    when(mockTS1.isEmpty()).thenReturn(true);
+    when(mockTS2.fire(1)).thenReturn(true);
+
+    // Act
+    ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    verify(mockTS2, times(1)).fire(anyInt());
+  }
+
+  @Test
+  public void fireTorpedo_Single_Fail() {
+    // Arrange
+    when(mockTS1.fire(1)).thenReturn(false);
+    when(mockTS2.fire(1)).thenReturn(true);
+
+    // Act
+    ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    verify(mockTS2, never()).fire(anyInt());
+  }
+
+  @Test
+  public void fireTorpedo_Single_BothEmpty() {
+    // Arrange
+    when(mockTS1.isEmpty()).thenReturn(true);
+    when(mockTS2.isEmpty()).thenReturn(true);
+
+    // Act
+    ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    verify(mockTS1, never()).fire(anyInt());
+    verify(mockTS2, never()).fire(anyInt());
+  }
+
 }
